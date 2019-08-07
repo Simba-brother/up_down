b44b7caaee3be4590ba83051f18a23d9e723ebea
Simba-brother <2196664077@qq.com>
2019-08-07 20:37:50 +0800

    8月7

--- CallChainExperiment/solidity_workspace/testTaint.sol
+++ CallChainExperiment/solidity_workspace/testTaint.sol
@@ -2,49 +2,79 @@ contract A {
     function aaaa(){
 
     }
+    function bbbb(address _addr){
+
+    }
 }
 contract Victim {
     //A a = new A();
     A a;
-    A aa;
+    A aa = A(0x2bd2326c993dfaef84f696526064ff22eba5b362);
     A aaa;
     address ddv;
     address owner;
     address c;
     modifier onlyOwner(){
-        owner == msg.sender;
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier mamingliang(){
+        require(msg.sender == owner);
         _;
     }
     constructor(address _addr){
-        a = A(_addr);
+        //a = A(msg.sender);
+        owner = msg.sender;
     }
-    function method_1(address _attack) {
-        a = A(_attack);
-        // A a = A(_attack);
+    function method_4(){
+       // method_2();
+    }
+    function method_2(){
+        require(msg.sender == owner);
+        msg.sender.send(10);
+       method_3();
+    }
+    function method_3(){
+        
+        //require(msg.sender == owner);
         a.aaaa();
-        // A(_attack).aaaa();
-        // _attack.call();
-        aaa = a;
-        aaa.aaaa();
-        c.call.value(10)();
     }
-    function d_set(address _attack)  {
-        ddv = _attack;
+    function method_1(address _attack)  {
+        
+        a = A(_attack);  /// 这时不应该为taint
+         
+        //aa.bbbb(msg.sender);
+       // a.aaaa();
+        //msg.sender.call();
+        // aa = A(_attack);
+        // aa.aaaa();
+        // A a = A(_attack);
+        // a.aaaa();
+        // // A(_attack).aaaa();
+        // // _attack.call();
+        // a = A()
+        // aaa = a;
+        // aaa.aaaa();
+        // c.call.value(10)();
     }
-    function calllll(address _attack) public returns(address){
-
-       // d(ddv);
-    }
-    // function b(address _addr) private onlyOwner() {
-    //     a = A(_addr);
+    // function d_set(address _attack)  {
+    //     ddv = _attack;
     // }
+    // function calllll(address _attack) public returns(address){
 
-    // function c(address _addr) public {
-    //     a = A(_addr);
+    //    // d(ddv);
     // }
+    // // function b(address _addr) private onlyOwner() {
+    // //     a = A(_addr);
+    // // }
 
-    function d(address dd) private{
-	     aa = A(dd);
-         aa.aaaa();
-    }
+    // // function c(address _addr) public {
+    // //     a = A(_addr);
+    // // }
+
+    // function d(address dd) private{
+	//      aa = A(dd);
+    //      aa.aaaa();
+    // }
 }
